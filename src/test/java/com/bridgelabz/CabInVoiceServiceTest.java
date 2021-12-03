30eb4dbf5948e6204fb9a98d3d2824d8d716e695
Janani <vsvprema@gmail.com>
2021-12-04 00:30:20 +0530

    Added :Take Multiplr Rides and calculate total  fare

--- src/test/java/com/bridgelabz/CabInVoiceServiceTest.java
+++ src/test/java/com/bridgelabz/CabInVoiceServiceTest.java
@@ -1,9 +1,15 @@
 package com.bridgelabz;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabInVoiceServiceTest {
+    CabInVoiceGenerator inVoiceGenerator = null;
+    @Before
+    public void setUp() throws Exception{
+        inVoiceGenerator = new CabInVoiceGenerator();
+    }
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
         CabInVoiceGenerator inVoiceGenerator=new CabInVoiceGenerator();
@@ -11,5 +17,23 @@ public class CabInVoiceServiceTest {
         int time = 5;
         double fare = inVoiceGenerator.calculateFare(distance, time);
         Assert.assertEquals(25, fare, 25.0);
+    }
+
+    @Test
+    public void givenLessDistanceAndFare_ShouldReturnMinFare(){
+        CabInVoiceGenerator inVoiceGenerator = new CabInVoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = inVoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5.0, fare, 0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] ride1 = {new Ride(2.0, 5),
+                new Ride(0.1, 1),
+        };
+        CabInVoiceGenerator inVoiceGenerator = new CabInVoiceGenerator();
+        Assert.assertEquals(30,0.0);
     }
 }
